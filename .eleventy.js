9a6692d4eaecaca494a4ad7342a4f7a99f0fd74d
thisisgonnatakeawhile <104325157+thisisgonnatakeawhile@users.noreply.github.com>
2022-04-27 18:22:03 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,23 +5,23 @@ const md = markdownIt({
     html: true,
     linkify: true,
   });
-module.exports = eleventyConfig => {
-    eleventyConfig.addLayoutAlias("base", "layouts/base.liquid");
-    eleventyConfig.addGlobalData("layout", "base");
+
+module.exports = function (eleventyConfig){
     eleventyConfig.addPlugin(footnotes, { /* … */ });
     eleventyConfig.addFilter("markdown", function (markup) {
       return md.renderInline(markup);
     });
-};
-module.exports = function (eleventyConfig){
+
     eleventyConfig.addPassthroughCopy('./src/css/');
     eleventyConfig.addPassthroughCopy('./src/js/');
-    eleventyConfig.addPassthroughCopy('./src/assets');
+    // eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addCollection("orderedPost", function (collection) {
         return collection.getFilteredByTag("post").sort((a, b) => {
           return a.data.order - b.data.order;
-        });
+        })
+        .filter(post => !Boolean(post.data.featured));
     });
+
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
     });
