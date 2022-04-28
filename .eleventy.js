101e9a87e9a6a3cb4b166cf0453e1a7478551b8c
Peter deHaan <pdehaan@hey.com>
2022-04-27 18:09:53 -0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,35 +1,36 @@
 const { DateTime } = require("luxon");
-const footnotes = require('eleventy-plugin-footnotes');
 const markdownIt = require("markdown-it");
-const md = markdownIt({
+
+module.exports = function (eleventyConfig) {
+  // Create our custom markdown-it instance.
+  const md = markdownIt({
     html: true,
     linkify: true,
-  })
-  .use(require('markdown-it-footnote'));
+  }).use(require("markdown-it-footnote"));
 
-module.exports = function (eleventyConfig){
-    eleventyConfig.addPlugin(footnotes, { /* … */ });
-    eleventyConfig.addFilter("markdown", function (markup) {
-      return md.renderInline(markup);
-    });
+  // Overwrite the built-in Markdown library with our custom instance.
+  eleventyConfig.setLibrary("md", md);
 
-    eleventyConfig.addPassthroughCopy('./src/css/');
-    eleventyConfig.addPassthroughCopy('./src/js/');
-    // eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addCollection("orderedPost", function (collection) {
-        return collection.getFilteredByTag("post").sort((a, b) => {
-          return a.data.order - b.data.order;
-        })
-        .filter(post => !Boolean(post.data.featured));
-    });
+  eleventyConfig.addPassthroughCopy("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/js/");
 
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
-    });
-    return{
-        dir:{
-            input: "src",
-            output: "public",
-        }
-    };
-}
+  eleventyConfig.addCollection("orderedPost", function (collection) {
+    return collection
+      .getFilteredByTag("post")
+      .sort((a, b) => {
+        return a.data.order - b.data.order;
+      })
+      .filter((post) => !Boolean(post.data.featured));
+  });
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
